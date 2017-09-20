b5d8dfcffc543dbc0a07bfbf390a45a55f9032c0
negoPreto <negopreto@negopreto>
2017-09-20 17:28:01 -0300

    Refazendo o segundo exercicio com inserção ordenada por string

---
+++ diario2/treino/segundo2.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+typedef struct registro{
+  char nome[30];
+  int matricula;
+  struct registro* prox;
+  struct registro* ant;
+}Registro;
+
+typedef struct cab{
+  Registro* primeiro;
+  Registro* ultimo;
+  int quant;
+}Cab;
+
+Cab* iniciaCab(Cab* l){
+  l = (Cab*)malloc(sizeof(Cab));
+  l->primeiro = NULL;
+  l->ultimo = NULL;
+  l->quant = 0;
+  return l;
+}
+
+void inserePrimeiro(Cab* l, char tempNome[30], int tempMatricula){
+  Registro* novo = (Registro*)malloc(sizeof(Registro));
+  novo->prox = NULL;
+  novo->ant = NULL;
+  strcpy(novo->nome,tempNome);
+  novo->matricula = tempMatricula;
+
+  l->quant++;
+  l->primeiro = novo;
+  l->ultimo = novo;
+}
+
+void insereAluno(Cab* l, char tempNome[30], int tempMatricula){
+  int i;
+  Registro* p;
+  for(i = 0; i < l->quant; i++){
+    
+  }
+}
+
+
+
+int main(){
+  Cab* l;
+  l = iniciaCab(l);
+
+  if(l->primeiro == NULL){
+    inserePrimeiro(l,tempNome,tempMatricula);
+  }else{
+    insereAluno(l, tempNome, tempMatricula);
+  }
+
+  return 0;
+}
