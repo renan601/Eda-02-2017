fcdcda27b795d343f0c255840c8e0d904601336f
Rafael Makaha <rafael.makah@gmail.com>
2017-11-29 19:37:09 -0200

    Definidos valores para letras e criado struct para letras

--- trab2/corretorOrtografico.c
+++ trab2/corretorOrtografico.c
@@ -22,6 +22,40 @@
 #define ARQTEXTO_ERROLEITURA    4
 #define ERRO_DICIO_NAOCARREGADO 5
 
+/* definicao dos valores das letras */
+#define a 0
+#define b 1
+#define c 2
+#define d 3
+#define e 4
+#define f 5
+#define g 6
+#define h 7
+#define i 8
+#define j 9
+#define k 10
+#define l 11
+#define m 12
+#define n 13
+#define o 14
+#define p 15
+#define q 16
+#define r 17
+#define s 18
+#define t 19
+#define u 20
+#define v 21
+#define w 22
+#define x 23
+#define y 24
+#define z 25
+
+/* Structs */
+typedef struct letra {
+    int valor;
+    struct letra* prox;
+}Letra;
+
 /* Retorna true se a palavra estah no dicionario. Do contrario, retorna false */
 bool conferePalavra(const char *palavra) {
 
